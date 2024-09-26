0dcf1836e1851e612773741ac8ef2c19d2cb3d7d
ammane07 <92978222+ammane07@users.noreply.github.com>
2024-09-26 15:05:32 +0530

    Final logging

--- api.js
+++ api.js
@@ -2,23 +2,12 @@ exports.handler = async (event, context) => {
   if (event.httpMethod === 'POST') {
     try {
       //console.log('event is : ', event)
-      console.log('event body is : ', event.body)
+      //console.log('event body is : ', event.body)
       // Parse the incoming JSON payload from the request body
       const requestBody = JSON.parse(event.body);
       console.log('requestBody is : ', requestBody)
-
-      // Save the data to a database or perform other necessary operations
-      // ...
-
-     // const requestBody = event.body;
-
-     // console.log(requestBody);
-     // console.log(requestBody.mode);
-
-      //let variableX = requestBody.mode;
+      
       let variableX = requestBody;
-
-
       return {
         headers: {
             "content-type": "application/json",
@@ -28,11 +17,7 @@ exports.handler = async (event, context) => {
       body: JSON.stringify({"Data message is ":variableX}),
       };  
 
-      // Return a success response
-      //return {
-      //  statusCode: 200,
-      //  body: JSON.stringify({ message: 'POST request processed successfully'.concat(' ', requestBody) }),
-      //};
+     
     } catch (error) {
       // Return an error response if there was an issue processing the request
       return {
